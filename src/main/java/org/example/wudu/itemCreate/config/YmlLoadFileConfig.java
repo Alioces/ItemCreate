1891ea7279443bf4d28325c9ea376796f4e22495
AlicoegT <1832321966@qq.com>
2025-12-12 19:13:51 +0800

    基础代码

--- src/main/java/org/example/wudu/itemCreate/config/YmlLoadFileConfig.java
+++ src/main/java/org/example/wudu/itemCreate/config/YmlLoadFileConfig.java
@@ -1,11 +1,18 @@
 package org.example.wudu.itemCreate.config;
 
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.example.wudu.itemCreate.item.CustomItem;
 
 import java.io.File;
 import java.util.List;
 
 public class YmlLoadFileConfig implements LoadFileConfig {
+
+    private final YamlConfiguration yamlConfiguration;
+
+    YmlLoadFileConfig(YamlConfiguration yamlConfiguration){
+        this.yamlConfiguration = yamlConfiguration;
+    }
     @Override
     public List<CustomItem> readFileFolder(File file) {
         return null;
