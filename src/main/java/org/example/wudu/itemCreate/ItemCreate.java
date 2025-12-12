13a368fe5f0e1fd7d7fc613023a967313ff5b778
AlicoegT <1832321966@qq.com>
2025-12-12 14:06:03 +0800

    创建

--- src/main/java/org/example/wudu/itemCreate/ItemCreate.java
+++ src/main/java/org/example/wudu/itemCreate/ItemCreate.java
@@ -7,7 +7,7 @@ public final class ItemCreate extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getLogger().info("hello-world");
     }
 
     @Override
